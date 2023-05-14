886fdbe031839a74eeac105a204bf4e7a605347c
snvskomalshanmuka <shanmukavenkatkomal@gmail.com>
2023-05-14 16:57:30 +0530

    created

---
+++ src/Addition.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Addition {
+    public static void main(String[] args){
+        Scanner k=new Scanner(System.in);
+        int a=k.nextInt();
+        int b=k.nextInt();
+        int c=a+b;
+        System.out.println("a+b is " +c);
+    }
+}
---
+++ src/Subtract.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Subtract {
+    public static void main(String[] args){
+        Scanner p = new Scanner(System.in);
+        int num1=p.nextInt();
+        int num2=p.nextInt();
+        int sub=num1-num2;
+        System.out.println("the output is "+sub);
+
+
+    }
+}
