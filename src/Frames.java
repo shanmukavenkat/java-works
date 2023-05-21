850877629d05d20e032c72f3cf7529483012205b
snvskomalshanmuka <shanmukavenkatkomal@gmail.com>
2023-05-21 20:11:42 +0530

    frame

---
+++ src/Frames.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+class Frames {
+    public static void main(String args[]){
+        Frame f= new Frame("title");
+        //created title
+        f.setSize(400,400);
+        //frame width and height are arranged
+        f.setVisible(true);
+        //by default frame visible is false
+        //now applied to true
+    }
+}
