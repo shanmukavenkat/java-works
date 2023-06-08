df4b18b7ff44ea5b23cee310f62ab943e513a91a
snvskomalshanmuka <shanmukavenkatkomal@gmail.com>
2023-06-08 23:42:08 +0530

    class and object examples

---
+++ src/Cre.java
@@ -0,0 +1,9 @@
+import tutorials.*;
+public class Cre {
+    public static void main(String[] args){
+        New cls= new New();
+        int c=cls.add(5,15);
+        System.out.println(c);
+    }
+}
+
