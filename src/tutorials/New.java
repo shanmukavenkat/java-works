4e47ac31cc67b8b8bb3105a279f6e16cbae5013f
snvskomalshanmuka <shanmukavenkatkomal@gmail.com>
2023-06-08 23:43:05 +0530

    package user defined

---
+++ src/tutorials/New.java
@@ -0,0 +1,12 @@
+package tutorials;
+
+public class New {
+    public static int add(int a,int b)
+    {
+        return a+b;
+
+    }
+    public static void main(String[] args){
+        System.out.println("new created package");
+    }
+}
