afeb1132fba68463a1b7d5af864cee775310d37a
snvskomalshanmuka <shanmukavenkatkomal@gmail.com>
2023-06-09 11:49:27 +0530

    methods

---
+++ src/Methods.java
@@ -0,0 +1,11 @@
+public class Methods
+{
+    public static void reaponding()
+    {
+        System.out.println("we respond to your question");
+    }
+    public static void main(String[] args)
+    {
+             reaponding();
+    }
+}
