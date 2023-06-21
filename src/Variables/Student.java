53bc06ad80548661b3a76ca3674e47ea7913ff42
snvskomalshanmuka <shanmukavenkatkomal@gmail.com>
2023-06-21 09:49:48 +0530

    variables

---
+++ src/Variables/Student.java
@@ -0,0 +1,10 @@
+package Variables;
+
+public class Student {
+
+    public static void main(String[] args){
+//here we created is called methods
+    }
+    //Here we are creating the instance variable
+    //instance variable can be created in the class but not in the method
+}
---
+++ src/Variables/Variables.java
@@ -0,0 +1,18 @@
+package Variables;
+
+public class Variables {
+    public static void main(String[] args){
+        int pincode=560037;
+        String name;
+        name="snvs";
+        System.out.println("name is "+name);
+        name="komal";
+        System.out.println("name is "+name);
+
+        System.out.println("pincoed is "+pincode);
+
+    }
+    //Define a variable means only creating a variable
+    //Initializing a variable means giving a value to the variable
+    //Here we can override the value of a variable
+}
