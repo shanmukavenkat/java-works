003d9ee7af16d873eedf75c140ab5871b6a506c1
snvskomalshanmuka <shanmukavenkatkomal@gmail.com>
2023-06-21 09:46:05 +0530

    constructor

---
+++ src/Construct.java
@@ -0,0 +1,18 @@
+class Construct {
+    public static void main(String[] args){
+//created main method
+        Construct P  = new Construct();
+        Construct P1  = new Construct();
+        Construct P2  = new Construct();
+
+    }
+    //here we are creating constructor
+    Construct(){//zero parameter constructer
+        System.out.println("this is constructor");
+    }
+    Construct(int i,String name){//parameterized constructer
+        System.out.println("this is constructor");
+    }
+
+}
+
