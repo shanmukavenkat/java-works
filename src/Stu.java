46d307026603c2247b1be6d66e37f75bb92ccfec
snvskomalshanmuka <shanmukavenkatkomal@gmail.com>
2023-06-21 09:46:24 +0530

    constructor

---
+++ src/Stu.java
@@ -0,0 +1,16 @@
+public class Stu {
+    //creating instance variable
+    int a;
+    public static void main(String[] args){
+        Stu s1= new Stu();
+        // s1.display();
+        //here it prints by default values of a making 0
+    }
+    Stu( ){
+        a=10;//assiging value to the instance variable
+        System.out.println(a);
+    }
+   // void display(){
+     //   System.out.println(a);
+   // }
+}
