4e2490acba55ca58b722b5e0a54ab9a2510b8c99
snvskomalshanmuka <shanmukavenkatkomal@gmail.com>
2023-06-24 18:25:15 +0530

    static method

---
+++ src/methods/Staticn.java
@@ -0,0 +1,10 @@
+package methods;
+
+public class Staticn {
+    static  void add(int a, int b){
+        System.out.println("addition is:"+(a+b));
+    }
+    public static void main(String[] args){
+        add(10,78);
+    }
+}
