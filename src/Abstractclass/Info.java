5a1a91654d7ea947b216df2acd11d86b17f2f70f
snvskomalshanmuka <shanmukavenkatkomal@gmail.com>
2023-06-25 13:52:07 +0530

    Abstract

---
+++ src/Abstractclass/Info.java
@@ -0,0 +1,20 @@
+package Abstractclass;
+
+public class Info {
+    public static void main(String[] args){//declaration method
+        //definition method
+        int c= 234;
+        System.out.println(c);
+        //these are known as concrete method
+        //void main is return type method
+        //here these void main is called as concrete method
+
+    }
+
+        void add(){
+            System.out.println("string");
+        }
+        //having same different methods but having same void return type method is known
+        //concrete class
+
+}
