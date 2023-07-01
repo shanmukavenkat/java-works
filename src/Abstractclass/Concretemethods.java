3f2ff246f5166ef296ab1a37e8f6bff528a5ca1f
snvskomalshanmuka <shanmukavenkatkomal@gmail.com>
2023-07-01 10:45:50 +0530

    concretemethod and concrete class

---
+++ src/Abstractclass/Concretemethods.java
@@ -0,0 +1,16 @@
+package Abstractclass;
+
+public class Concretemethods {
+    public static void main(String args[]){
+        System.out.println("concrete methods");
+    }
+    //having the  concrete methods is known as concrete class
+    //and having the only method with declaration is known as concrete method
+    void sum(){
+        System.out.println("concrete method");
+    }
+    void  cal(){
+        System.out.println("concrete method");
+    }
+
+}
