028c21c3e13fbc49161daff1a570f2786f504f84
snvskomalshanmuka <shanmukavenkatkomal@gmail.com>
2023-07-03 19:08:40 +0530

    Files in java

--- src/File/Files.java
+++ src/File/Files.java
@@ -4,8 +4,12 @@ import java.io.*;
 public class Files {
     public static void main(String[] args){
         try{
-            //to create a fil we use the try block
-             File myobj=new File("d:\\Demo.txt");
+            //to create a file we use the try and catch block
+            //here we have used the -->createNewFile() method
+            //and we mentioned the path of the file
+            // File myobj=new File("d:\\KOMAL\\Demo.txt"); to create in directory
+
+            File myobj=new File("d:\\KOMAL\\Demo.txt");
              if (myobj.createNewFile()){
                  System.out.println("File is created");
              }else{
